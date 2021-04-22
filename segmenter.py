1fff164e36b4258fb95c3391b13a7629972bd9e7
Xanda Schofield <xanda@cs.hmc.edu>
2021-04-21 22:34:24 -0700

    Initial commit

---
+++ segmenter.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import sys
+import argparse
+from tokenizer import tokenize
+
+def my_best_segmenter(token_list): 
+    """ TODO: Replace this with an improved sentence segmenter. """
+    pass
+
+def baseline_segmenter(token_list):
+    all_sentences = []
+    this_sentence = []
+    for token in token_list:
+        this_sentence.append(token)
+        if token in ['.', ':', ';', '!', '?']:
+            all_sentences.append(this_sentence)
+            this_sentence = []
+    return all_sentences
+
+def write_sentence_boundaries(sentence_list, out):
+    """ TODO: Write out the token numbers of the sentence boundaries. """
+    pass
+
+def main(args):
+    pass
+
+if __name__ == "__main__": 
+    parser = argparse.ArgumentParser(description="Sentence Segmenter for NLP Lab")
+    parser.add_argument('--textfile', "-t", metavar="FILE", type=argparse.FileType('r'),
+                        required=True, help="Unlabled text is in FILE.")
+    parser.add_argument("--hypothesis_file", "-y", metavar="FILE", type=argparse.FileType('w'),
+                        required=False, default=sys.stdout,
+                        help="Write hypothesized boundaries to FILE")
+    args = parser.parse_args()
+    main(args)
